dcf8703f93f21e78bd8ca7325f95907e0c15dcba
Aarushi Bhatia <aarushibhatia073@gmail.com>
2025-01-07 16:55:15 +0530

    password encrypt

--- backend/index.js
+++ backend/index.js
@@ -7,4 +7,8 @@ const PORT = 8000;
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
+app.get("/", (req, res) =>{
+  res.json();
+})
+
 app.listen(PORT, () => console.log(`Server has started at PORT:${PORT}`))
