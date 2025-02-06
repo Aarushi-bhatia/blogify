45eaa13e26b227448429d3c6ae9d09055195d8f8
Aarushi Bhatia <aarushibhatia073@gmail.com>
2025-02-06 14:59:40 +0530

    schema done

--- backend/index.js
+++ backend/index.js
@@ -1,32 +1,123 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
-mongoose.connect("mongodb+srv://aarushibhatia27:VLawYgTAFqL1M0z2@cluster0.qmeu0.mongodb.net/")
+mongoose.connect(
+  "mongodb+srv://aarushibhatia27:VLawYgTAFqL1M0z2@cluster0.qmeu0.mongodb.net/"
+);
 
-const User = mongoose.model('Users', {name: String, email: String, password: String});
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  bio: {
+    type: String,
+  },
+  profilePicture: {
+    type: String,
+  },
+  posts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+    },
+  ],
+});
 
-app.post('/signup', async function(req, res){
-  const username = req.body.username;
-  const password = req.body.password;
-  const name = req.body.name;
+const PostSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  subtitle: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId, ref:'User', required: true
+  },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date },
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment",
+    },
+  ],
+  likes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  tags: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Tag'
+  }]
+});
 
-  const existingUser = await User.findOne({ username: email});
-  if(existingUser){
-    return res.status(400).send("Username already exists");
-  }
+const CommentSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  post: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Post",
+    required: true,
+  },
+  createdAt: { type: Date, default: Date.now },
+  likes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+});
 
-  const user = new User({
-    name: name,
-    email: username,
-    password: password
-  });
-  
-  user.save();
-  res.json({
-    "msg": "User created successfully"
-  })
-})
+const TagSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  post: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Post",
+    required: true,
+  },
+});
 
+const User = mongoose.model('User', UserSchema);
+const Post = mongoose.model('Post', PostSchema);
+const Comment = mongoose.model('Comment', CommentSchema);
+const Tag = mongoose.model('Tag', TagSchema);
+
+module.exports = {
+  User,
+  Post,
+  Comment
+}
